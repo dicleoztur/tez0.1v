5f589da29c28347ff379889cc63ac2e491a3e720
dicle <dicleoztur@gmail.com>
2014-09-03 17:31:48 +0300

    before-big-test

--- learning/shell.py
+++ learning/shell.py
@@ -15,8 +15,8 @@ import performance_evaluation_crossval
 def run_cross_validation(outrootpath):
     k = 5
     #scorespath = os.path.join(outrootpath, "scores")
-    learner_selected.conduct_cross_validation_notest(k, outrootpath)
-    #performance_evaluation_crossval.evaluate_cross_validation(outrootpath)
+    #learner_selected.conduct_cross_validation_notest(k, outrootpath)
+    performance_evaluation_crossval.evaluate_cross_validation(outrootpath)
     
     
 if __name__ == "__main__":
